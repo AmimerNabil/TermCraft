c04db78e1a74166e213af36ccafd355ead900723
Amimer Nabil <amimernabil@gmail.com>
2024-10-18 22:00:20 -0400

    feat: removed useless elements in main

--- main.go
+++ main.go
@@ -13,11 +13,6 @@ import (
 var App tview.Application
 
 func main() {
-	// out, serr, err := python.UnInstallPythonVersion("2.1.3")
-	// fmt.Println("out " + out)
-	// fmt.Println("err " + serr)
-	// fmt.Println(err)
-
 	if !slices.Contains(configs.SupportedOS, runtime.GOOS) {
 		log.Panic("Unsupported OS...")
 	}
